af662dde60633df13e010dea599608d3dd08d54e
Harrisonh <harrisonh@educ.college-montreal.qc.ca>
2022-09-29 08:17:03 -0400

    Tp2

---
+++ TP2.py
@@ -0,0 +1,18 @@
+"""
+HENRY HARRISON
+23 SEPTEMBRE 2022
+CODE: DEVINETTE DE CHIFFRE
+
+"""
+import random
+
+x = random(0, 100)
+
+nb_essai = 0
+
+print("J'ai choisi un nombre entre 0 et 100.")
+print("a vous de le deviner!")
+
+input = print("Entrez votre essai:  ")
+
+nb_essai = nb_essai + 1
